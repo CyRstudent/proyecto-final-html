da32b46b139fc4a0f3ad92b02a6ddc4c63c1e2ca
CyRstudent <a1433hsaez@iesmarmediterraneo.org>
2022-05-06 16:44:56 +0200

    feat: Welcome with info introduced

--- scripts/validar.js
+++ scripts/validar.js
@@ -7,10 +7,12 @@ function validar() {
 	const pass1 = document.getElementById('pw1').value;
 	const pass2 = document.getElementById('pw2').value;
 	const salida = document.getElementById('salida');
+	const nombre = document.getElementById('name');
+	const apellidos = document.getElementById('surname');
 	if (pass1 !== pass2) {
 		salida.innerHTML= '<br><i>Las contraseñas no coinciden</i></br>';
 	} else {
-		salida.innerHTML = '<br>Bienvenido!</br>';
+		salida.innerHTML = `<br>Bienvenido, ${nombre} ${apellidos}!</br>`;
 	}
 	return salida.innerHTML;
 };
